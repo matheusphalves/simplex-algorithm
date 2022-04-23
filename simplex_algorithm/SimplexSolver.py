9e8009ac2c983500658674eaeb23742a43f413b1
Matheus Phelipe <matheusphalves@gmail.com>
2022-04-23 17:16:52 -0300

    implemeting simplex algorithm

--- simplex_algorithm/SimplexSolver.py
+++ simplex_algorithm/SimplexSolver.py
@@ -1,5 +1,6 @@
 import time
 import numpy as np
+import json
 from simplex_algorithm.Interaction import Interaction
 
 class SimplexSolver():
@@ -33,50 +34,87 @@ class SimplexSolver():
 
         while self.is_done is False and self.iterations <= self.max_iteractions:
             print(f'Starting iteration {self.iterations}')
-            self.make_iteration(set_B, set_N)
-            set_B = [2,3,4]
-            Set_N = [0, 1]
+            b_inverted, list_z_index, matrix_XB = self.make_iteration(set_B, set_N)
             print(f'Z score:{self.previous_interactions[-1].z_value}')
-            print(f'XB matrix: {self.previous_interactions[-1].matrix_XB}')
             self.iterations += 1
+            if self.is_done is False:
+                index_entering = self.compare_tuples(list_z_index)[1]
+                leaving_data = self.calculate_leaving_variable(b_inverted, index_entering)
+                alpha_data = self.calculate_alpha(leaving_data, matrix_XB, set_B)
+                index_leaving = set_B[alpha_data[0][1]]
+                print(f'XB({index_leaving}) is leaving. XB({index_entering}) is entering')
+                set_B = self.swap_matrices_index(set_B, index_leaving, index_entering)
+                set_N = self.swap_matrices_index(set_N, index_entering, index_leaving)
 
         end = time.time()
         print(f'Simplex Iteractions finished: {end-start}')
+        self.generate_report()
         return self.previous_interactions
 
     
     def add_slack_variable(self):
         pass
-        
+
+    def swap_matrices_index(self, set_list, leaving, entering):
+        aux = []
+        for index in set_list:
+            if index == leaving:
+                aux.append(entering)
+            else:
+                aux.append(index)
+        return aux
+
 
     def make_iteration(self, set_B, set_N):
         try:
             matrix_B = self.matrix_a[:, set_B]
             matrix_N = self.matrix_a[:, set_N]
             matrix_CB = self.matrix_c[:, set_B]
-            self.calculate(matrix_B=matrix_B, matrix_CB=matrix_CB, set_B=set_B, set_N=set_N, matrix_N=matrix_N)
-            if self.is_done is False:
-                self.calculate_leaving_variable()
-                self.calculate_alpha()
+            return self.calculate(matrix_B=matrix_B, matrix_CB=matrix_CB, set_B=set_B, set_N=set_N, matrix_N=matrix_N)
         except Exception as ex:
             print(f'Failed on make_iteration method\n{ex}')
 
     def calculate(self, matrix_B, matrix_CB, set_B, set_N, matrix_N):
         b_inverted = np.linalg.inv(matrix_B)
         u = np.matmul(matrix_CB, b_inverted)
-        xb = np.matmul(b_inverted, self.matrix_b)
+        matrix_XB = np.matmul(b_inverted, self.matrix_b)
         z = np.matmul(u, self.matrix_b)[:,0][0]
 
-        list_z_index = [np.matmul(u, self.matrix_a[:, index])[0] - self.matrix_c[:, index][0] for index in set_N]
+        list_z_index = [(np.matmul(u, self.matrix_a[:, index])[0] - self.matrix_c[:, index][0], index) for index in set_N]
 
-        self.is_done = all(i >= 0 for i in list_z_index)
-        self.previous_interactions.append(Interaction(matrix_B, set_B, matrix_N, set_N, xb, z))
+        self.previous_interactions.append(Interaction(matrix_B, set_B, matrix_N, set_N, matrix_XB, z))
+        self.is_done = all(i[0] >= 0 for i in list_z_index)
 
-    def calculate_leaving_variable(self):
-        pass
+        return b_inverted, list_z_index, matrix_XB
 
-    def calculate_alpha(self, l_set):
-        pass
+    def compare_tuples(self, tuple): #(z_value, index)
+        smaller = tuple[0]
+        for index in range(1, len(tuple)):
+            if tuple[index][0] < smaller[0]:
+                smaller = tuple[index]
+        return smaller
+
+    def calculate_leaving_variable(self, b_inverted, index_N):
+        y_index_N = np.matmul(b_inverted, self.matrix_a[:, index_N])
+        greater_0 = []
+        for index in range(len(y_index_N)):
+            if y_index_N[index] > 0:
+                greater_0.append(index)
+
+        return y_index_N, greater_0
+
+    def calculate_alpha(self, leaving_data, matrix_XB, set_B):
+        alpha_list = [(matrix_XB[index][0]/ leaving_data[0][index], index) for index in leaving_data[1]]
+        smaller = alpha_list[0]
+        index_leaving = set_B[0]
+        for index in range(1, len(alpha_list[0])):
+            if(alpha_list[index][0]<smaller[0]):
+                smaller = alpha_list[index]
+                index_leaving = set_B[index]
+
+        return smaller, index_leaving #retorna posição que deve ser removida
 
     def generate_report(self):
-        pass
+        #dict_list = [item.__dict__ for item in self.previous_interactions]
+        #return json.dumps(dict_list)
+        print(self.previous_interactions)
--- test.py
+++ test.py
@@ -1,13 +1,26 @@
 from simplex_algorithm.SimplexSolver import SimplexSolver
 
 
+#matrix_A = [
+#    [1, 0.5, 1, 0, 0],
+#    [6, 2, 0, 1, 0],
+#    [5, 4, 0, 0, 1]]
+#matrix_B = [44, 250, 280]
+#matrix_C = [60, 40, 0, 0, 0]
+
+#matrix_A = [
+#    [1, 0, 1, 0, 0],
+#    [0, 1, 0, 1, 0],
+#    [3, 2, 0, 0, 1]]
+#matrix_B = [4, 6, 18]
+#matrix_C = [3, 5, 0, 0, 0]
+
 matrix_A = [
-    [1, 0.5, 1, 0, 0],
-    [6, 2, 0, 1, 0],
-    [5, 4, 0, 0, 1]]
-matrix_B = [44, 250, 280]
-matrix_C = [60, 40, 0, 0, 0]
+    [1, 3, 1, 0],
+    [1, 1, 0, 1]]
+matrix_B = [8, 4]
+matrix_C = [1, 2, 0, 0, 0]
 
-simplex = SimplexSolver(matrix_a=matrix_A, matrix_b=matrix_B, matrix_c=matrix_C, max_iteractions=1)
+simplex = SimplexSolver(matrix_a=matrix_A, matrix_b=matrix_B, matrix_c=matrix_C, max_iteractions=10)
 
-simplex.start([2,3,4], [0, 1])
+simplex.start([2,3], [0, 1])
